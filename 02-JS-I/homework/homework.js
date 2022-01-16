52cda75ffbfe3b597821b6d593ea3823144ae254
iansoul1 <arkevdel@gmail.com>
2022-01-16 12:49:43 -0600

    Problema15

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -154,19 +154,21 @@ function elevar(num, exponent) {
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
-  
+  if (resta>=0.5){
+    return 
+    
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-  
+  return math.cell(num); 
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  
+  return math.random();
 }
 
 function esPositivo(numero) {
@@ -174,7 +176,14 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
+  if (nuemro===0){
+    return("Falso");
+  }
+  if (numero>0){
+  }
+  else {
+    return ("Es Negativo");
+  }
 }
 
 function agregarSimboloExclamacion(str) {
