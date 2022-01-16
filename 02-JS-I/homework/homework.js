74a194ea72fff3d3b0024fa390b1ec78e35c3044
iansoul1 <arkevdel@gmail.com>
2022-01-16 10:39:24 -0600

    Problema7

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -75,15 +75,23 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (str1.length===str2.length)}
+    return true;
 }
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num<90){
+    return true;
+  }
+    else{
+      return false;
+    }
 }
+
+  
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
