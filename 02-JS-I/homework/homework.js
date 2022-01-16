e6c8ae055b8c5e83de98605d79f7b127b1a744c1
iansoul1 <arkevdel@gmail.com>
2022-01-16 09:45:17 -0600

    Problema2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -35,7 +35,9 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  x=8, y=10
+  suma=x+y
+  return suma;
 }
 
 function resta(x, y) {
