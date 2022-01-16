3fffc29d44aa469337daca70075d6784100bfcac
iansoul1 <arkevdel@gmail.com>
2022-01-16 11:20:52 -0600

    Problema11

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -123,8 +123,14 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num % 2 === 1){
+    return true;
+    }
+    else{
+      return false;
+    }
 }
+
 
 function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
